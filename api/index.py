5f102e74c5d436b4440602c5b2a83df300220ced
Sven <iamsven2005@gmail.com>
2024-03-06 15:22:41 +0800

    mongo test 1

--- api/index.py
+++ api/index.py
@@ -1,11 +1,27 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 app = Flask(__name__)
+uri = "mongodb+srv://iamsven2005:BIbQ7Lgmq6ZbdU14@cluster0.1amm90a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+client = MongoClient(uri, server_api=ServerApi('1'))
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+db = client['EC']
+collection = db['rewards']
 @app.route('/')
 def home():
     return 'Hello, World!'
 
 @app.route('/about')
 def about():
-    return 'About'
+    cursor = collection.find({})
+    about_data = [doc for doc in cursor]
+    return render_template('about.html', about_data=about_data)
+
+
+    
+
